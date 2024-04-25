fc982155fe9aacc977c776a6d823ab255fe104be
Masha <masha@MacBook-Air.local>
2024-04-25 18:00:15 +0200

    Object Property Enumeration and Deletion

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//Task 1: Object Property Manipulation
 const person = {
   firstName: "John",
   lastName: "Doe",
@@ -43,3 +44,41 @@ Object.defineProperty(person, "address", {
   value: {},
   writable: true,
 });
+
+//Task 2: Object Property Enumeration and DeletionTask 2: Object Property Enumeration and Deletion
+const product = {
+  name: "Laptop",
+  price: 1000,
+  quantity: 5,
+};
+
+Object.defineProperty(product, 'price', {
+  writable: false, 
+  enumerable: false, 
+});
+
+Object.defineProperty(product, 'quantity', {
+  writable: false, 
+  enumerable: false ,
+});
+
+function getTotalPrice (product) {
+  const isPriseEnumerable = Object.getOwnPropertyDescriptor(product, 'price').enumerable;
+  const isQuantityEnumerable = Object.getOwnPropertyDescriptor(product, 'quantity').enumerable;
+  let totalPrice;
+  if (!isPriseEnumerable && !isQuantityEnumerable) {
+    totalPrice = product.price * product.quantity;
+  } else {
+    throw new Error("Check emumerable of price or quantity properties.")
+  }
+  
+  return totalPrice;
+}
+
+function deleteNonConfigurable (objectToModify, propertyToDelete) {
+  if (Object.getOwnPropertyDescriptor(objectToModify, propertyToDelete).configurable) {
+    delete objectToModify[propertyToDelete]; 
+  } else {
+    throw new Error(`It's impssible to delete nonconfigurable property`);
+  }
+}
